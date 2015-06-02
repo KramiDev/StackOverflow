7f6400d123a37235f451ba59d4e62fa458cc28a1
kramidev <krami.dev@gmail.com>
2015-06-02 16:08:26 +0600

    add dependent destroy in question, add min length for title of question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
-  it { should validate_length_of(:title).is_at_most(150) }
+  it { should validate_length_of(:title).is_at_least(3).is_at_most(150) }
 
-  it { should have_many(:answers) }
+  it { should have_many(:answers).dependent(:destroy) }
 
 end
