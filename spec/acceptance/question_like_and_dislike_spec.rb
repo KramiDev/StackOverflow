7bb1aea5a348759ff9e6fa822cf7fc631ec9d07d
kramidev <krami.dev@gmail.com>
2015-06-25 00:03:35 +0600

    Added acceptace/question_like_and_dislike_spec.rb, fix questions/show(added 2 links 'like' and 'dislike')

---
+++ spec/acceptance/question_like_and_dislike_spec.rb
@@ -0,0 +1,59 @@
+require_relative '../acceptance_helper'
+
+feature 'Question like or dislike', %q{
+  I order to mark question
+  As an user
+  I want to select good or bad question
+} do
+
+  given(:users){ create_list(:user, 2) }
+  given(:question){ create(:question, user: users[0]) }
+
+  scenario 'User try to like or dislike his question' do
+
+    sign_in(users[0])
+    visit question_path(question)
+
+    within '.votes' do
+      expect(page).to_not have_link 'like'
+      expect(page).to_not have_link 'dislike'
+    end
+
+  end
+
+  scenario 'User try to like other\'s question' do
+    
+    sign_in(users[1])
+    visit question_path(question)
+
+    within '.votes' do
+      click_on 'like'
+      expect(page).to have_content 'Likes: 1'
+    end
+
+  end
+
+  scenario 'User try to dislike other\'s question' do
+
+    sign_in(users[1])
+    visit question_path(question)
+
+    within '.votes' do
+      click_on 'dislike'
+      expect(page).to have_content 'Likes: -1'
+    end
+
+  end
+
+  scenario 'Non-authenticated user try to like or dislike some question' do
+
+      visit question_path(question)
+
+      within '.votes' do
+        expect(page).to_not have_link 'like'
+        expect(page).to_not have_link 'dislike'
+      end
+
+  end
+
+end
